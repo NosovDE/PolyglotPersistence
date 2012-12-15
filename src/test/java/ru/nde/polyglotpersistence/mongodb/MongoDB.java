5fa380e55c6dee976f465120c1320da9e542dcec
NosovDE <NosovDE@gmail.com>
2012-12-15 23:52:30 +0400

    under construction

---
+++ src/test/java/ru/nde/polyglotpersistence/mongodb/MongoDB.java
@@ -0,0 +1,13 @@
+package ru.nde.polyglotpersistence.mongodb;
+
+/**
+ * @Author: Dmitriy E. Nosov <br>
+ * @Date: 15.12.12, 22:21 <br>
+ * @Company: Korus Consulting IT<br>
+ * Revision:    \$Id$ <br>
+ * @Description: <br>
+ */
+public class MongoDB {
+
+
+}
