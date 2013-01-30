6ef4393fa580936d83aa76c935750854de7f4975
NosovDE <NosovDE@gmail.com>
2013-01-30 19:32:53 +0400

    Добавлена поддержка Groovy

---
+++ src/main/groovy/ru/nde/AppGroovy.groovy
@@ -0,0 +1,13 @@
+package ru.nde
+
+/**
+ * Author:      Dmitriy E. Nosov <br>
+ * Date:        30.01.13, 14:58 <br>
+ * Company:     Korus Consulting IT<br>
+ * Description:  <br>
+ */
+class AppGroovy {
+    def test = {
+
+    }
+}
